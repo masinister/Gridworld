2d3284bb476cbf8e82e6177ca69ca7580306f1c7
mtdicicco <mtdicicco@wpi.edu>
2021-02-04 16:16:00 -0500

    cover time, naming

--- utils.py
+++ utils.py
@@ -27,13 +27,14 @@ def params(g):
     walls = [n for n in list(g.nodes) if g.degree(n)==0]
     g.remove_nodes_from(walls)
     return {"diameter": diameter(g),
+            "cover time": cover_time(g),
             "conductance": conductance(g),
-            "avg_eccentricity": avg_eccentricity(g),
+            "avg eccentricity": avg_eccentricity(g),
             "connectivity": connectivity(g),
             "efficiency": efficiency(g),
-            "min_eigenvalue": min_eigenvalue(g),
-            "max_eigenvalue": max_eigenvalue(g),
-            "closeness_vitality": closeness_vitality(g)}
+            "min eigenvalue": min_eigenvalue(g),
+            "max eigenvalue": max_eigenvalue(g),
+            "closeness vitality": closeness_vitality(g)}
 
 def dict_error(q1, q2):
     return np.sum([np.sum(np.abs(q1[k] - q2[k])) for k in q1.keys()])
