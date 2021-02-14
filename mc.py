3f49dbbeae693eb7fe70637332295d4cfd34aace
mtdicicco <mtdicicco@wpi.edu>
2021-02-14 15:14:33 -0500

    removed sarsa, add convergence check

--- mc.py
+++ mc.py
@@ -1,9 +1,9 @@
 import numpy as np
 import random
-from collections import defaultdict
+from collections import defaultdict, deque
 from tqdm import tqdm
 
-def mellowmax(Q, state, omega = 1.0):
+def mellowmax(Q, state, omega = 10.0):
     a = Q[state]
     prob = np.exp(a) / np.sum(np.exp(a))
     return np.random.choice(len(prob), p = prob)
@@ -14,48 +14,27 @@ def epsilon_greedy(Q, state, epsilon = 0.1):
         return random.randint(0, len(a)-1)
     return np.argmax(a)
 
-def sarsa(env, n_episodes, gamma=0.95, alpha=0.1, epsilon=1.0):
-    # OFF policy TD control
-    Q = defaultdict(lambda: np.zeros(env.action_space.n))
-    learning_curve = []
-    q = 0
-    for e in tqdm(range(n_episodes)):
-        epsilon *= 0.99
-        done = False
-        state = env.reset()
-        action = epsilon_greedy(Q, state, epsilon)
-        while not done:
-            next_state, reward, done, info = env.step(action)
-            next_action = epsilon_greedy(Q, next_state, epsilon)
-
-            td_target = reward + gamma * Q[next_state][next_action]
-            td_error = td_target - Q[state][action]
-            Q[state][action] += alpha * td_error
-            q += alpha * td_error
-            state = next_state
-            action = next_action
-
-        learning_curve.append(q)
-    return Q, learning_curve
-
 def q_learning(env, n_episodes, gamma=0.95, alpha=0.1, epsilon=1.0):
-    # ON policy TD control
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
     learning_curve = []
+    last30 = deque(maxlen = 30)
     q = 0
     for e in range(n_episodes):
         epsilon = max(epsilon * 0.99, 0.1)
         done = False
         state = env.reset()
         while not done:
-            action = epsilon_greedy(Q, state, epsilon)
+            action = mellowmax(Q, state)
             next_state, reward, done, info = env.step(action)
 
             td_target = reward + gamma * np.amax(Q[next_state])
             td_error = td_target - Q[state][action]
             Q[state][action] += alpha * td_error
-            q += alpha * td_error
             state = next_state
 
+            q += alpha * td_error
         learning_curve.append(q)
+        last30.append(q)
+        if len(last30) == 30 and max(last30) < 1e-4:
+            break
     return Q, learning_curve
