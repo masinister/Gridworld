b45e8cb95524b9733fcd30283ff3a1907115b96f
mtdicicco <mtdicicco@wpi.edu>
2021-02-14 15:14:55 -0500

    plot distance from optimal Q

--- plot.py
+++ plot.py
@@ -9,9 +9,11 @@ def plot(legend_var):
     std_cmap = (cmap - np.mean(cmap)) / np.std(cmap)
     norm_cmap = (cmap - np.min(cmap)) / (np.max(cmap) - np.min(cmap))
 
+    plt.figure(1)
+
     for i in range(len(data)):
         v = np.real(data[i][0][legend_var])
-        plt.plot(data[i][-1], label = np.float("{:.3f}".format(v)), c=pallete(std_cmap[i]))
+        plt.plot(data[i][-2], label = np.float("{:.3f}".format(v)), c=pallete(std_cmap[i]))
 
     plt.title(legend_var)
     plt.xlabel("Episodes")
@@ -20,7 +22,15 @@ def plot(legend_var):
     m = plt.cm.ScalarMappable(cmap=pallete)
     m.set_array(cmap)
     plt.colorbar(m)
+
+    plt.figure(2)
+    y = [row[-1] for row in data]
+    plt.scatter(cmap, y)
+    plt.title(legend_var)
+    plt.xlabel(legend_var)
+    plt.ylabel("Distance from optimal Q function")
+
     plt.show()
 
 if __name__ == "__main__":
-    plot("avg_eccentricity")
+    plot("avg eccentricity")
