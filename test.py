33302fa2562e2ea00fca0542752c8602f2bfe9e5
mtdicicco <mtdicicco@wpi.edu>
2021-02-22 12:37:14 -0500

    plot learning curve

--- test.py
+++ test.py
@@ -11,15 +11,19 @@ from basegraphs import *
 d = (11,11)
 g = fourrooms(*d)
 add_random_edges(g, n=3)
-# print(params(g))
+print(params(g))
 
 env = gym.make('graphworld-v0', graph = g, dim = d)
 nA = env.action_space.n
 opt_Q = env.optimal_Q()
 
 Q = defaultdict(lambda: np.random.rand(nA))
-Q, lc = q_learning(env, n_episodes = 300, gamma = 0.95, alpha = 0.1)
-print(dict_error(opt_Q, Q))
+Q, lc = q_learning(env, n_episodes = 1000)
+print("error:", dict_error(opt_Q, Q), dict_ratio(opt_Q, Q))
+
+plt.figure(1)
+plt.plot(lc)
+plt.show()
 
 for i in range(100):
     done = False
