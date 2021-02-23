7485865d06dba280b1bb7ac0546e7ef88d5e0179
mtdicicco <mtdicicco@wpi.edu>
2021-02-23 17:58:53 -0500

    change to kleinberg edges

--- experiment.py
+++ experiment.py
@@ -11,7 +11,7 @@ from utils import *
 from plot import plot
 from basegraphs import *
 
-ray.init()
+ray.init(num_cpus = 6)
 
 d = (11,11)
 data = []
@@ -21,13 +21,14 @@ basegraphs = [fourrooms] * num_trials
 @ray.remote
 def run_one_trial(g):
     import gym_graphworld
-    add_random_edges(g, n = np.random.randint(1, 5))
+    add_kleinberg_edges(g, n = np.random.randint(1, 5), r=1)
 
     env = gym.make('graphworld-v0', graph = g, dim = d)
     nA = env.action_space.n
     opt_Q = env.optimal_Q()
+    target = np.sum([np.sum(np.abs(opt_Q[k])) for k in opt_Q.keys()])
 
-    Q, lc = q_learning(env, n_steps = 1e6)
+    Q, lc = q_learning(env, n_steps = 1e6, target_Q = target)
     return params(g), lc, dict_error(opt_Q, Q), dict_ratio(opt_Q, Q)
 
 print("Starting experiment:")
