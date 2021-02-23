11b84cb58be253274a669052ad5204bc8adb4517
mtdicicco <mtdicicco@wpi.edu>
2021-02-23 17:58:18 -0500

    tune

--- mc.py
+++ mc.py
@@ -9,16 +9,16 @@ def boltzmann(Q, state, tau=0.5):
     prob /= np.sum(prob)
     return np.random.choice(len(prob), p = prob)
 
-def epsilon_greedy(Q, state, epsilon = 0.5):
+def epsilon_greedy(Q, state, epsilon = 0.25):
     a = Q[state]
     if random.uniform(0,1) < epsilon:
         return random.randint(0, len(a)-1)
     return np.argmax(a)
 
-def q_learning(env, n_steps, gamma=0.95, alpha=0.05):
+def q_learning(env, n_steps, gamma=0.95, alpha=0.25, target_Q = 1, tol = 0.1):
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
     learning_curve = []
-    last30 = deque(maxlen = 30)
+    tail = deque(maxlen = 1000)
     q = 0
     step = 0
     while step < n_steps:
@@ -35,9 +35,9 @@ def q_learning(env, n_steps, gamma=0.95, alpha=0.05):
             state = next_state
 
             q += alpha * td_error
-            learning_curve.append(q)
-        last30.append(q)
-        error = np.abs(last30[-1] - last30[0])
-        if len(last30) == 30 and error < 0.01:
+            if step % 100 == 0:
+                learning_curve.append(q / target_Q)
+                tail.append(q)
+        if len(tail) == 1000 and np.abs(tail[-1] - tail[0]) < tol:
             break
     return Q, learning_curve
